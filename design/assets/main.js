e6003a24462d2fb36960476810a7ffb3638fa1e6
Zearf <s3924496@rmit.edu.vn>
2022-04-24 12:26:01 +0700

    Update design

--- design/assets/main.js
+++ design/assets/main.js
@@ -1,23 +1,35 @@
-// double click on the heart icon
-$(".fa-heart").dblclick(function () {
-  $(".notification-bubble").show(400);
-});
+var heart_click = document.querySelectorAll(".heart");
+for (i = 0; i < heart_click.length; i++) {
+  let click = heart_click[i];
+  click.onclick = function () {
+    heart_click_fuc(click);
+  };
+}
 
-$(document).on("scroll", function () {
-  if ($(document).scrollTop() > 50) {
-    $(".navigation").addClass("shrink");
+function heart_click_fuc(element) {
+  if (element.classList.contains("liked") === true) {
+    element.classList.remove("liked", "fas");
+    element.classList.add("icon-size", "far");
   } else {
-    $(".navigation").removeClass("shrink");
+    element.classList.remove("icon-size", "far");
+    element.classList.add("liked", "fas");
   }
-});
+}
 
-var search = document.getElementById("search-bar");
-search.addEventListener("keydown", function (e) {
-  if (e.keyCode === 13) {
-    validate(e);
+var bookmark_click = document.querySelectorAll(".bookmark");
+for (i = 0; i < bookmark_click.length; i++) {
+  let click = bookmark_click[i];
+  click.onclick = function () {
+    bookmark_click_fuc(click);
+  };
+}
+
+function bookmark_click_fuc(element) {
+  if (element.classList.contains("fas") === true) {
+    element.classList.remove("fas");
+    element.classList.add("far");
+  } else {
+    element.classList.remove("far");
+    element.classList.add("fas");
   }
-});
-
-function validate(e) {
-  alert("Validated");
 }
