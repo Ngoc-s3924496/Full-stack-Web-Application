1506866b92171258b9b2b877304197a94c4a2279
Zearf <s3924496@rmit.edu.vn>
2022-05-17 23:15:47 +0700

    Final

---
+++ www/php/login_admin.php
@@ -0,0 +1,16 @@
+<?php if(!isset($_SESSION)) { 
+  session_start(); 
+}  ?>
+
+<?php
+include_once("functions.php");
+if (isset($_POST['login'])) {
+    $email = clean_text($_POST['email_login']);
+    $password = clean_text($_POST['password_login']);
+    if ($email == "admin" and $password == "admin"){
+        header("Location: ../manage_user.php");
+    } else {
+        $_SESSION['error_login'] = "Wrong email or password";
+        header("Location: ../login_admin_page.php");
+    }
+}
